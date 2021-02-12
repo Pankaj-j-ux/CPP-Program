b05093d75484ccc59469ceb8524950226d0c9a46
Pankaj <442001ps@gmail.com>
2021-02-12 22:56:19 +0530

    12-FEB-2021

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size){
+
+    for (int i = 0; i < size-1; i++){
+        
+        for (int j = 0; j < size-i-1; j++){
+            
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }    
+        }
+        
+    }
+
+    for (int  i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+        
+
+    return;
+}
+
+int main(){
+
+    int size;
+    cout<<"ENTER THE SIZE OF ARRAY : ";
+    cin>>size;
+    cout<<endl;
+
+    int arr[size];
+    cout<<"ENTER THE INTERGER VALUES FOR ARRAY."<<endl;
+    cout<<"NOTE : Values must be seperated by space and Total values must be less than Size of Array."<<endl;
+    
+    for (int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
+
+    cout<<endl;
+    cout<<"INPUTED ARRAY IN SORTED ORDER : ";
+    bubbleSort(arr, size);
+
+    return 0;
+}
